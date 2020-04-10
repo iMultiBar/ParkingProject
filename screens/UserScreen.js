5af0bfca82b7005258a704ee43bea5892dff40c7
Deathsight <57475781+Deathsight@users.noreply.github.com>
2020-04-10 13:54:01 +0300

    Updated UserScrean
    fixed some simple issues,
    
    now it shows reservation,
    subscription
    
    added some animation and style

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -1,50 +1,72 @@
+//@refresh reset
 import React,{ useState, useEffect} from 'react';
-import { ScrollView, StyleSheet,Text, Picker, Button,View } from 'react-native';
+import { ScrollView, StyleSheet,Text, Picker,View,Image } from 'react-native';
 import db from '../db';
 
 import firebase from "firebase/app";
 import "firebase/auth";
 
+import SettingPage from "./SettingsScreen"
+import moment from "moment"
+
+//react elements:
+import { Card , Icon, Divider , Button, Avatar   } from 'react-native-elements';
+
+//react animatable:
+import * as Animatable from "react-native-animatable";
+
+
 export default function UserScreen() {
+
     const [user , setUser] = useState(false)
     const [serviceList , setServiceList] = useState([])
     const [service , setService] = useState(false)
     const [reservation, setReservation] = useState([])
     const [subscription, setSubscription] = useState(false)
-
+    const [updateResVar ,setUpdateResVar] = useState(null)
 
 
     useEffect(()=>{
         GetUser()
         getServices()
     },[])
-    
+    //...............
+
     useEffect(() => {
         let temp = []
-        if(user){
+            console.log("fetching data for res")
             db.collection("reservation").doc(firebase.auth().currentUser.uid).onSnapshot(snapShot => {
                     temp.push(snapShot.data())
                     console.log("fetching reservation data", snapShot.data())
                 }
             )
+            console.log("Done", temp)
             if(temp && temp != []){
                 setReservation(temp)
                 console.log("reservation:", temp)
             }
-        }
     },[])
+
+    const EndRes = () => {
+         db.collection("reservation").doc(firebase.auth().currentUser.uid).delete()
+    }
+
     const GetUser = async () => {
-        const User =await db.collection("users").doc(firebase.auth().currentUser.uid).get()
+        console.log("Getting user")
+        const User = await db.collection("users").doc(firebase.auth().currentUser.uid).get()
         setUser(User.data())
+        console.log("User Found:",User.data())
         const Sub = await db.collection("users").doc(firebase.auth().currentUser.uid).collection("subscription").doc("sub").get()
         console.log(Sub.data())
         if(Sub.data()){
             setSubscription(Sub.data());
         }
     }
-    const getServices = () => {
+
+    const getServices = async ()  => {
         let temp = []
-        db.collection('services').get()
+        console.log("Getting Services")
+        await db.collection('services').get()
         .then((snapshot) => {
           snapshot.forEach((doc) => {
             console.log(doc.id, '=>', doc.data());
@@ -54,49 +76,94 @@ export default function UserScreen() {
         .catch((err) => {
           console.log('Error getting documents', err);
         });
+        console.log("Services found:",temp) 
         setServiceList(temp)
-        
     }
+
+    const ExtendRes = (res) => {
+        var temp = res.endTime; 
+        var status = moment(temp).add(5, 'hours').format('YYYY-MM-DD hh:mm:ss');
+        db.collection("reservation")
+        .doc(firebase.auth().currentUser.uid)
+        .update({
+            endTime:status
+        });
+    }
+
   return (
       user ? 
         <View style={styles.container}>
-                {/* <img source={{ uri:`${user.photoURL}`} }/> */}
-                <Text>{user.displayName}</Text>
+            <View style={{padding:15,flexDirection:"row"}}>
+                {/*
+                        
+                */}
+                   
+                <View style={{ alignSelf:"flex-start",flexDirection:"row",marginRight:"60%"}}>
+                    <Avatar
+                        rounded
+                        source={{ uri: user.photoURL }}
+                    />
+                    <Text style={{padding:8}}>{user.displayName}</Text>
+                </View>
+                <Button
+                icon={
+                        <Icon
+                        name='settings-helper'
+                        type='material-community'
+                        color='#517fa4'
+                    />
+                }
+                type="clear"
+                />
+
+       
+            </View>
+
+ 
+
+            {/*
+                
+            */}
+            <Divider style={{ backgroundColor: 'blue' }} />
+
+
+
+
             <ScrollView >
 
-                {/* // User Service Tab */}
-                <View style={{borderColor:"black",borderWidth:3,borderStyle:"solid", marginBottom:20,padding:5}}>
-                    <Text>Request Service</Text>
-                    <Picker
-                        selectedValue={""}
-                        style={{height: 50, width: 100}}
-                        onValueChange={(itemValue, itemIndex) =>
-                            setService(itemValue)
-                        }>
+                {/* Request Service */}
+                <Card title="Request Service">
+                        <Picker
+                        selectedValue={service}
+                        style={{height: 50, width: 350}}
+                        onValueChange={(itemValue) => setService({itemValue})}
+                        >
                             {
-                            serviceList ? 
+
+                                
+                            serviceList && serviceList != undefined ? 
                                 serviceList.map( (item, index) =>
                                         <Picker.Item key={index} label={item} value={item} />
-                                    )
+                                )
                             :
-                               null
+                            null
                             }
                     </Picker>
-                </View>
+                </Card>
 
-
-                 {/* User Parking Reservation List Tap */}
-                <View style={{borderColor:"black",borderWidth:3,borderStyle:"solid", marginBottom:20,padding:5}}>
-                    <Text>Reservation List</Text>
+                {/* User Parking Reservation List Tap */}
+                <Card title="Reservation List">
                     <ScrollView>
-
                         { 
-                         reservation ?
+                         reservation && reservation != undefined ?
                          
                             <View>
-                                {reservation.map((item, index) => 
+                                {
+                                reservation.map((item, index) => 
                                         <View key={index}>
                                             <Text> Parking Location: { item.parking.location } Start Time: {item.startTime } End Time: { item.endTime }</Text>
+                                            <Button onPress={() => ExtendRes(item)}/>
+                                            <Button title="Cancel Reservation" onPress={() => EndRes()} />
                                         </View>
                                 )}
                                 <Button title="Reserve"/>
@@ -105,18 +172,18 @@ export default function UserScreen() {
                             :
                                 <>
                                     <Text> Empty</Text>
-                                    <Button title="Reserve"/>
+                                    <Button title="Reserve" />
                                 </>
                         }
                     </ScrollView>
-                </View>
+                </Card>
 
 
                 {/* // Your subscription Tap */}
-                <View style={{borderColor:"black",borderWidth:3,borderStyle:"solid", marginBottom:20,padding:5}}>
-                        <Text>Your subscription</Text>
+                <Card title="Your subscription">
+                        
                         {
-                            subscription ?
+                            subscription && subscription != undefined ?
                                 <View>
                                     {/* <Text>subscription End Date: {subscription.endDate}</Text>
                                    <Text>subscription Level: {subscription.level} </Text> */}
@@ -128,15 +195,26 @@ export default function UserScreen() {
                             :
                                 <>
                                     <Text>You are not Subscriped</Text>
-                                    <Button title="Subscripe" />
+                                    <Button title="Subscribe" onPress={<SettingPage />} />
                                 </>
                         }
-                </View>
+                </Card>
 
             </ScrollView>
         </View> 
     :
-    <Text>Loadiing...</Text>
+    
+    <View style={{flex:1,paddingTop:15,justifyContent:"center",alignSelf:"center"}}>
+        
+        <Animatable.Text animation="slideInDown" iterationCount={5} direction="alternate">Loading...</Animatable.Text>
+        <Animatable.View animation="pulse" easing="ease-out" iterationCount="infinite" style={{ textAlign: 'center' }}>
+        <Button
+            title="Loading button"
+            loading 
+            type="clear"
+        />
+        </Animatable.View>
+    </View>
   );
 }
 
