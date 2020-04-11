325506f6fe1072891ddf69e189ce22f89c7797e7
Deathsight <57475781+Deathsight@users.noreply.github.com>
2020-04-11 14:48:20 +0300

    Cleaned the home screen
    added a new package : image slider

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,17 +5,47 @@ import { createStackNavigator } from 'react-navigation-stack';
 import MapScreen from './MapScreen';
 import ReservationScreen from './Reservation';
 import { createAppContainer } from 'react-navigation';
+import { SliderBox } from "react-native-image-slider-box";
+
+
+import News from "./NewsScreen"
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
-function HomeScreen() {
+function HomeScreen(props) {
+
+    const [images, setImages] = useState({
+        images: [
+            "https://www.cna.nl.ca/images/cna-fb-share.jpg", 
+            "https://media-cdn.tripadvisor.com/media/photo-s/09/12/9b/d3/marine-atlantic-ferry.jpg",
+            "https://www.cna-qatar.com/Campus%20Facilities/CNA-Q%20Entrance.jpg",
+            "https://media.glassdoor.com/l/bd/98/1e/3b/cnaq-campus-view.jpg",
+          ]
+    })
+
+
+
     return (
-       <Text>this is the home page</Text>
+        <View>
+            <SliderBox images={images.images} 
+            onCurrentImagePressed={index => console.warn(`image ${index} pressed`)}
+            autoplay={true}
+            circleLoop
+        />
+            <ScrollView>
+                <News />
+            </ScrollView>
+            <TouchableOpacity style={{padding:5, paddingRight:20,alignSelf:"flex-end"}} onPress={() => props.navigation.push("News")}>
+                <Text>Read More</Text>
+            </TouchableOpacity>
+        </View>
+
     )
 }
 
 HomeScreen.navigationOptions = {
-    tabBarLabel: "Home",
+    title: "Home",
     tabBarIcon: () => (
         <AntDesign name="home" size={30} color="blue" />
     )
