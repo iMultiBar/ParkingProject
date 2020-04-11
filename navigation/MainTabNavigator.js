3ebc7310d6734aa1535951f00f86345e8890c91b
Deathsight <57475781+Deathsight@users.noreply.github.com>
2020-04-11 12:40:54 +0300

    Removed all unneceessery taps,
    removed all the taps that is not shared with all users for later navigation implementation

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -14,15 +14,14 @@ import "firebase/auth";
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import ReportScreen from '../screens/ReportScreen';
-import LinksScreen from '../screens/LinksScreen';
 import UserScreen from '../screens/UserScreen';
-import TestScreen from '../screens/TestScreen';
+import RequestCleaner from '../screens/TestScreen';
 import CleanerScreen from '../screens/CleanerScreen'; // import the cleanerScreen.js to use it in the StackNavigator
 import NewsScreen from '../screens/NewsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 import SuggestionsScreen from '../screens/SuggestionsScreen';
-import carriersScreen from '../screens/carriersScreen';
-// import ValetScreen from '../screens/ValetScreen';
+import CarriersScreen from '../screens/carriersScreen';
+import ValetScreen from '../screens/ValetScreen'
 import ReservationScreen from '../screens/Reservation';
 import MapScreen from '../screens/MapScreen';
 
@@ -42,7 +41,7 @@ const config = Platform.select({
 */
 const HomeStack = createStackNavigator(
   {
-    Home: HomeScreen,
+    Home: HomeScreen, News: { screen: NewsScreen} ,
   },
   config
 );
@@ -59,6 +58,31 @@ HomeStack.navigationOptions = {
 
 HomeStack.path = '';
 ////////////////////////////////////////////////////////////////////////
+
+const UserScreenStack = createStackNavigator(
+  {
+    UserScreen: UserScreen, Map: { screen: MapScreen }, 
+    Settings: { screen: SettingsScreen  }, 
+    Reservation: { screen: ReservationScreen }, 
+    Suggestions: { screen:  SuggestionsScreen},
+    Cleaner: { screen: CleanerScreen },
+    Carriers: { screen: CarriersScreen },
+    Valet: { screen: ValetScreen },
+    RequestCleaner: {screen: RequestCleaner}
+  },
+  config
+);
+
+UserScreenStack.navigationOptions = {
+
+  tabBarLabel: 'UserScreen',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
+  ),
+};
+
+UserScreenStack.path = '';
+////////////////////////////////////////////////////////////////
 const MapStack = createStackNavigator(
   {
     Map: { screen: MapScreen }, Reservation: { screen: ReservationScreen }
@@ -78,227 +102,15 @@ MapStack.navigationOptions = {
 
 MapStack.path = '';
 ////////////////////////////////////////////////////////////////////////
-const LinksStack = createStackNavigator(
-  {
-    Links: LinksScreen,
-  },
-  config
-);
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
-};
-
-LinksStack.path = '';
-//////////////////////////////////////////////////////////
-const ReportStack = createStackNavigator(
-  {
-    Report: ReportScreen,
-  },
-  config
-);
-
-ReportStack.navigationOptions = {
-  tabBarLabel: 'Report',
-  tabBarIcon: ({ focused }) => (
-    <MaterialIcons focused={focused} name={'report'} />
-  ),
-};
-
-ReportStack.path = '';
-//////////////////////////////////////////////////////////
-/*
-  This is the SuggestionsStack object. It handles the bottom navigation for the SuggestionsScreen
-  below there is also the navigationOptions for the SuggestionsStack, and inside of that is the title
-  and icon of the Bottom Tab. the stack navigator is then added to the tabNavigator object.
-*/
-const SuggestionsStack = createStackNavigator(
-  {
-    Suggestions: SuggestionsScreen,
-  },
-  config
-);
-
-SuggestionsStack.navigationOptions = {
-  tabBarLabel: 'Suggestions',
-  tabBarIcon: ({ focused }) => (
-    <FontAwesome focused={focused} name={'question'} />
-  ),
-};
-
-SuggestionsStack.path = '';
-
-////////////////////////////////////////////////////////////////
-const TestStack = createStackNavigator(
-  {
-    Test: TestScreen,
-  },
-  config
-);
-
-TestStack.navigationOptions = {
-  tabBarLabel: 'Test',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
-};
-
-TestStack.path = '';
-////////////////////////////////////////////////////////////////
-/*
-  This is the NewsStack object. It handles the bottom navigation for the NewsScreen
-  below there is also the navigationOptions for the NewsStack, and inside of that is the title
-  and icon of the Bottom Tab. the stack navigator is then added to the tabNavigator object.
-*/
-const NewsStack = createStackNavigator(
-  {
-    News: NewsScreen,
-  },
-  config
-);
-
-NewsStack.navigationOptions = {
-  tabBarLabel: 'News',
-  tabBarIcon: ({ focused }) => (
-    <Entypo focused={focused} name={'news'} />
-  ),
-};
-
-NewsStack.path = '';
-//////////////////////////////////////////////////////////////
-const SettingsStack = createStackNavigator(
-  {
-    Settings: SettingsScreen,
-  },
-  config
-);
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
-};
-////////////////////////////////////////////////////////////////
-// this const creates stack navigator which uses the import apove to display when the user click on it.
-const CleanerStack = createStackNavigator(
-  {
-    Cleaner: CleanerScreen,
-  },
-  config
-);
-// this naigationoptions allows to edit the tab it self for here. i added a tabBarLable which give a name to the tab
-CleanerStack.navigationOptions = {
-  tabBarLabel: 'CleanerScreen',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
-};
-
-CleanerStack.path = '';
-////////////////////////////////////////////////////////////////
-// const ValetStack = createStackNavigator(
-//   {
-//     Valet: ValetScreen,
-//   },
-//   config
-// );
-// // this naigationoptions allows to edit the tab it self for here. i added a tabBarLable which give a name to the tab
-// ValetStack.navigationOptions = {
-//   tabBarLabel: 'ValetScreen',
-//   tabBarIcon: ({ focused }) => (
-//     <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-//   ),
-// };
-
-// ValetStack.path = '';
-//////////////////////////////////////////////////////////////
-
-SettingsStack.path = '';
-
-
-const UserScreenStack = createStackNavigator(
-  {
-    UserScreen: UserScreen,
-  },
-  config
-);
-
-UserScreenStack.navigationOptions = {
-
-  tabBarLabel: 'UserScreen',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'} />
-  ),
-};
-
-UserScreenStack.path = '';
-////////////////////////////////////////////////////////////////
-const CarriersStack = createStackNavigator(
-  {
-    Carriers: carriersScreen,
-  },
-  config
-);
-
-CarriersStack.navigationOptions = {
-  tabBarLabel: 'CarriersScreen',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'} />
-  ),
-};
-
-CarriersStack.path = '';
 
 // here the const get all the const names apove and put the them in tabnaigator to show them in the bottom on the screen because we used (createBottomTabNavigator)
 const tabNavigator = createBottomTabNavigator({
   HomeStack,
   MapStack,
-  TestStack,
-  SettingsStack,
-  UserScreenStack,
-  LinksStack,
-  NewsStack,
-  SuggestionsStack,
-  CleanerStack,
-  CarriersStack,
-  ReportStack,
-  // ValetStack
+  UserScreenStack
 });
 
-// work in progress...
-
-// if(firebase.auth().currentUser === null){
-//  console.log("still did not log in")
-// }else{..
-  
-//   let role;
-//   console.log(firebase.auth().currentUser.uid)
-//   db.collection("users").doc(firebase.auth().currentUser.uid).onSnapshot(snapShot => {
-//     role = snapShot.data().role;
-    
-//   }
-//   )
-//   console.log("sssss",role)
-//   if(role === "student"){
-//     tabNavigator = createBottomTabNavigator({
-//       HomeStack,
-//       SettingsStack,
-//       UserScreenStack,
-//       NewsStack
-//     });
-//   } else{
-//     tabNavigator = createBottomTabNavigator({
-//       HomeStack,
-//       LinksStack,
-//       TestStack,
-//       SettingsStack,
-//     });
-//   }
-// }
 
 
 tabNavigator.path = '';
