5e5f3b3c0904888e8dcd5ad3f88bc296aee84895
Deathsight <57475781+Deathsight@users.noreply.github.com>
2020-04-11 16:52:25 +0300

    Clenaed and improved setting page
    you can now add cars and remove cars
    from the setting page,
    
    moving the subsctiption into a new page

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Image, Text, TextInput, Button, Picker } from "react-native";
+import { StyleSheet, View, Image, Text, TextInput, Picker, KeyboardAvoidingView } from "react-native";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/storage";
@@ -8,6 +8,12 @@ import db from "../db";
 import * as ImagePicker from "expo-image-picker";
 import { ScrollView } from "react-native-gesture-handler";
 
+//react animatable:
+import * as Animatable from "react-native-animatable";
+
+
+import { Input, Avatar,Button,Card } from "react-native-elements"
+
 export default function SettingsScreen() {
   const [hasCameraRollPermission, setHasCameraRollPermission] = useState(false);
   const [displayName, setDisplayName] = useState("");
@@ -15,7 +21,14 @@ export default function SettingsScreen() {
   const [photoURL, setPhotoURL] = useState("");
   const [subscription, setSubscription] = useState("")
   const [flag, setFlag] = useState(false);
-  const [userSub, setUserSub] = useState([])
+  const [userSub, setUserSub] = useState([]);
+  const [profile,setProfile] = useState(null);
+  const [selectedCar, setSelectedCar] = useState(null) ;
+
+  const [loading, setLoading] = useState(false);
+
+  const [carList , setCarList] = useState([]);
+  const [newCar, setNewCar] = useState(null);
 
   const askPermission = async () => {
     const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
@@ -31,17 +44,58 @@ export default function SettingsScreen() {
     setDisplayName(user.displayName);
     setPhotoURL(user.photoURL);
 
+    const usersCars = await db.collection("cars").doc(firebase.auth().currentUser.uid).get()
+    console.log("Fetching users Car List", usersCars.data())
+    if(usersCars.data()){
+        let cars = usersCars.data()
+        setCarList(cars.registerdCars)
+    }
+
+    const data = await db.collection("users").doc(firebase.auth().currentUser.uid).get()
+    setProfile(data.data())
+
     let check = await db.collection("users").doc(firebase.auth().currentUser.uid).collection("subscription").doc("sub").get()
     if(check.data()){
       setFlag(true)
       setUserSub(check.data())
     }
+    setLoading(true)
   };
 
   useEffect(() => {
     handleSet();
   }, []);
 
+  useEffect(()=> {
+
+  },[])
+
+  const handleNewCar = async (carPlate) => {
+    if(carList){
+        let temp = carList
+        if(carPlate){
+            temp.push(carPlate)
+            console.log(carPlate, "=>", temp)
+            await db.collection("cars").doc(firebase.auth().currentUser.uid).set({
+                registerdCars:temp
+            })
+        } else {
+            console.log("Wrong input")
+        }
+        getInfo()
+    } else {
+        console.log("registering new car Failed");
+    }
+    
+}   
+  const deleteCar = async (carPlate) => {
+    let temp = carList
+    let index = carList.indexOf(carPlate)
+    temp.splice(index, 1)
+    await db.collection("cars").doc(firebase.auth().currentUser.uid).set({
+      registerdCars:temp
+    })
+  }
 
 
   const handleSave = async () => {
@@ -112,25 +166,66 @@ export default function SettingsScreen() {
   
 
   return (
+    loading ? 
+    <KeyboardAvoidingView style={{flex:1}}>
     <ScrollView style={styles.container} keyboardShouldPersistTaps="always">
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: "gray",
-          borderWidth: 1,
-          fontSize: 24
-        }}
-        onChangeText={setDisplayName}
-        placeholder="Display Name"
-        value={displayName}
-      />
-      {photoURL !== "" && (
-        <Image style={{ width: 100, height: 100 }} source={{ uri: photoURL }} />
-      )}
-      <Button title="Pick Image" onPress={handlePickImage} />
-      <Button title="Save" onPress={handleSave} />
+
+<View style={{flexDirection:"row", margin:15}}>
+        <View style={{flex:1.5}}>
+            {photoURL !== "" && (
+                <Avatar rounded onPress={handlePickImage} showEditButton size="large" source={{ uri: photoURL }} />
+            )}
+        </View>
+        <View style={{flex:4,justifyContent:"center"}}>
+          <Input
+              onChangeText={setDisplayName}
+              placeholder="Display Name"
+              value={displayName}
+          />
+          <Button buttonStyle={{justifyContent:"flex-end"}} type="clear" title="Save" onPress={handleSave} />
+        </View>
+
+</View>
+<View style={{padding:5}}>
+  {profile ?
       <View>
-      {flag == false ? <View>
+        <Text style={{paddingLeft:8}}>Department:</Text>
+          <Input disabled value={profile.department}/>    
+        <Text style={{paddingLeft:8}}>Email Address:</Text>
+          <Input disabled value={profile.email}/>   
+        <Text style={{paddingLeft:8}}>Phone:</Text>
+          <Input disabled value={profile.phone}/>   
+        <Text style={{paddingLeft:8}}>Role:</Text> 
+          <Input disabled value={profile.role}/>    
+      </View> 
+        :
+      null
+  }
+  </View> 
+      
+<View>
+
+    <Card title="Manage Registered Cars">
+    { carList ? 
+        <Picker
+            selectedValue={selectedCar}
+            style={{ height: 50, width: '100%' }}
+            onValueChange={(itemValue, itemIndex) => setSelectedCar(itemValue)}
+          >
+              {carList.map((item,index) => 
+              <Picker.Item label={item} value={item} />
+            )}
+        </Picker>
+
+        :null
+    }
+      <Input placeholder="Car Plate Number" keyboardType={'numeric'} onChangeText={text => setNewCar(text)} />
+        <View style={{flexDirection:"row",justifyContent:"space-between",padding:5}}>
+          <Button title="Add Car" buttonStyle={{width:150}} onPress={() => handleNewCar(newCar)}/>
+          <Button title="Delete Car" buttonStyle={{width:150, backgroundColor:"red"}} onPress={() => deleteCar(selectedCar)}/>
+      </View>
+    </Card>
+      {/* {flag == false ? <View>
         <Text>
         Subscription 
         </Text>
@@ -154,11 +249,26 @@ export default function SettingsScreen() {
           <Text>Type: {userSub.type}</Text>
           <Text>Valet Point: {userSub.valetPoints}</Text>
           </View>
-          }
+          } */}
       
       </View>
-      <Button title="Logout" onPress={handleLogout} />  
+      <Button buttonStyle={{backgroundColor:"darkred"}} title="Logout" onPress={handleLogout} />  
     </ScrollView>
+    
+
+  </KeyboardAvoidingView>
+  :
+  <View style={{flex:1,paddingTop:15,justifyContent:"center",alignSelf:"center"}}>
+          
+    <Animatable.Text animation="slideInDown" iterationCount={5} direction="alternate">Loading...</Animatable.Text>
+    <Animatable.View animation="pulse" easing="ease-out" iterationCount="infinite" style={{ textAlign: 'center' }}>
+    <Button
+        title="Loading button"
+        loading 
+        type="clear"
+    />
+    </Animatable.View>
+  </View>
   );
 }
 
