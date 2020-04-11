9a87ae7919a2142f2bc943f5193e021b19f38979
haanianadeem4424 <57193683+haanianadeem4424@users.noreply.github.com>
2020-04-11 20:56:50 +0300

    reward screen

--- screens/RewardsScreen.js
+++ screens/RewardsScreen.js
@@ -26,7 +26,6 @@ import { user } from "firebase-functions/lib/providers/auth";
 
 export default function RewardsScreen() {
     const [points, setPoints] = useState(0);
-    const [counter, setCounter] = useState(0);
 
     const [expiryDuration, setExpiryDuration] = useState("");
     const [type, setType] = useState("");
@@ -47,6 +46,10 @@ export default function RewardsScreen() {
         });
     }, []);
 
+    useEffect(() => {
+        checkForUserRewards(); 
+    }, []); 
+
     console.disableYellowBox = true;
 
 
@@ -56,88 +59,57 @@ export default function RewardsScreen() {
         let reportData = check.data();
         let reportStatus = reportData.status;
 
-        if (reportData === null) {
+        if (reportData !== null) {
             setPoints(0);
-            db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").set({
+            await db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").update({
                 reward_points: points
             });
         }
 
-        else if (reportStatus === "unapproved") {
+        else if (reportStatus === "approved") {
             setPoints(0);
-            db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").set({
+            await db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").update({
                 reward_points: points
             });
         }
 
         else {
             setPoints(points + 1);
-            db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").set({
+            await db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").update({
                 reward_points: points
             })
         }
     }
 
 
-    const checkForUserRewards = () => {
-        let userRewardInfo = db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").get();
-        let userRewardData = userRewardInfo.data(); // This line is being undefined
-        let userRewardPoints = userRewardData.reward_points; // This line is being undefined
-
-        console.log("Current User Reward Info ", userRewardInfo);
-
+    const checkForUserRewards = async () => {
+        let userRewardInfo = await db.collection("users").doc(firebase.auth().currentUser.uid).collection("userRewards").doc("reward").get();
+        let userRewardData = userRewardInfo.data();
+        let userRewardPoints = userRewardData.reward_points;
 
         for (let i = 0; i < rewards.length; i++) {
             let rewardPoints = rewards[i].points;
-            if (userRewardPoints === rewardPoints) {
-                let rewardObject = rewards[i];
 
-                <View>
-                    <Card
-                        containerStyle={{ borderColor: "blue" }}
-                        image={require('../assets/images/car_wash.png')}
-                        title='CONGRATULATIONS!'
-                        imageStyle={{ height: 250 }}
-                    >
-                        <Text style={{
-                            marginBottom: 10, fontFamily: "serif",
-                            textAlign: "center", fontSize: 18, fontWeight: "bold"
-                        }}>
-                            You have received a reward for a {rewardObject.type} service.
-                            </Text>
-                        <Text style={{
-                            marginBottom: 10, fontFamily: "serif",
-                            textAlign: "center", fontSize: 18, fontWeight: "bold"
-                        }}>
-                            The expiry date for this reward is {rewardObject.expiry_duration}.
-                            </Text>
-                    </Card>
-                </View>
+            console.log("Current Reward Points ", rewardPoints);
+
+            console.log("User Points ", userRewardPoints);
+            console.log("Reward Points", rewardPoints);
+
+            if (userRewardPoints === rewardPoints) {
+                console.log("There is a reward");
+                setRewardStatus(true);
+                setType(rewards[i].type);
+                setExpiryDuration(rewards[i].expiryDuration);
             }
 
 
             else {
-                <View>
-                    <Card
-                        containerStyle={{ borderColor: "blue" }}
-                        image={require('../assets/images/no-reward.png')}
-                        title='SORRY!'
-                        imageStyle={{ height: 250 }}
-                    >
-                        <Text style={{
-                            marginBottom: 10, fontFamily: "serif",
-                            textAlign: "center", fontSize: 18, fontWeight: "bold"
-                        }}>
-                            You haven't received any rewards from our parking app system.
-                        </Text>
-                    </Card>
-                </View>
-
+                console.log("There is no reward");
+                setRewardStatus(false);
+                setType("");
+                setExpiryDuration("");
             }
         }
-
-
-
     }
 
 
@@ -153,7 +125,43 @@ export default function RewardsScreen() {
                 Rewards Screen!
             </Text>
 
-            <Button title="CHECK FOR REWARDS" onPress={checkForUserRewards} />
+            {rewardStatus === true ?
+                <View>
+                    <Card
+                        containerStyle={{ borderColor: "blue" }}
+                        image={require('../assets/images/car_wash.png')}
+                        title='CONGRATULATIONS!'
+                        imageStyle={{ height: 250 }}
+                    >
+                        <Text style={{
+                            marginBottom: 10, fontFamily: "serif",
+                            textAlign: "center", fontSize: 18, fontWeight: "bold"
+                        }}>
+                            You have received a reward for a {type} service.
+                                        </Text>
+                        <Text style={{
+                            marginBottom: 10, fontFamily: "serif",
+                            textAlign: "center", fontSize: 18, fontWeight: "bold"
+                        }}>
+                            The expiry date for this reward is {expiryDuration}.
+                                        </Text>
+                    </Card>
+                </View> :
+                <View>
+                    <Card
+                        containerStyle={{ borderColor: "blue" }}
+                        image={require('../assets/images/no-reward.png')}
+                        title='SORRY!'
+                        imageStyle={{ height: 250 }}
+                    >
+                        <Text style={{
+                            marginBottom: 10, fontFamily: "serif",
+                            textAlign: "center", fontSize: 18, fontWeight: "bold"
+                        }}>
+                            You haven't received any rewards from our parking app system.
+                </Text>
+                    </Card>
+                </View> }
 
         </View>
     )
