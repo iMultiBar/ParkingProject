e4b16d0e203bb94ef30179f38aa88d7b3aa039a7
Deathsight <57475781+Deathsight@users.noreply.github.com>
2020-04-11 13:24:37 +0300

    Added styling to the login page.

--- App.js
+++ App.js
@@ -12,13 +12,11 @@ import {
   StyleSheet,
   View,
   TextInput,
-  Button,
   Picker,
-  ProgressViewIOS,
-  Text
+  KeyboardAvoidingView,Text
 } from "react-native";
-import ReactNativePickerModule from "react-native-picker-module"
 import { Ionicons } from "@expo/vector-icons";
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 // import {
 //   Select,
@@ -26,19 +24,23 @@ import { Ionicons } from "@expo/vector-icons";
 // } from 'react-native-option-select';
  
 
+
+console.disableYellowBox = true;
 import AppNavigator from "./navigation/AppNavigator";
 
 import firebase from "firebase/app";
 import "firebase/auth";
 import db from "./db";
+
+
+import { Input,Card,Button  } from "react-native-elements"
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function App(props) {
- let pickerRef = null;
   const [isLoadingComplete, setLoadingComplete] = useState(false);
   const [user, setUser] = useState(null);
-  const [view, setView] = useState("register");
-  console.disableYellowBox = true;
+  const [view, setView] = useState("login");
+
   const [email, setEmail] = useState("");  
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
@@ -63,6 +65,7 @@ export default function App(props) {
   };
 
   const handleLogin = async () => {
+    console.log(email,password)
     await firebase.auth().signInWithEmailAndPassword(email, password);
     updateUserLogin();
   };
@@ -99,30 +102,175 @@ export default function App(props) {
     );
   } else if (!user && view === "login") {
     return (
-      <View style={styles.contentContainer}>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={setEmail}
-          placeholder="Email"
+      <KeyboardAvoidingView behavior="height" style={styles.contentContainer}>
+      <View style={{justifyContent:"center",flex:1}}>
+
+
+      <Card
+        image={require('./assets/images/frontpage.jpg')}>
+        <View style={{marginBottom: 10}}>
+        <Text>Your Email Address</Text>
+        <Input
+          placeholder='email@address.com'
+          onChangeText={(text) => setEmail(text)}
           value={email}
+          inputStyle={{paddingLeft:10}}
+          leftIcon={<Icon
+            name='user'
+            color='lightgray'
+            size={20}
+          />}
         />
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          onChangeText={setPassword}
-          placeholder="Password"
+        <Text>Password</Text>
+        <Input
+          placeholder='password'
           secureTextEntry={true}
+          textContentType="password"
+          onChangeText={(text) => setPassword(text)}
+          inputStyle={{paddingLeft:10}}
+          leftIcon={
+            <Icon
+              type="FontAwesome"
+              name='lock'
+              color='lightgray'
+              size={20}
+            />
+          }
           value={password}
         />
-    
-        <Button title="submit" onPress={handleLogin} />
-        <Button title="go to register" onPress={handleView} />
-      </View>
+        </View>
+        <Button
+          
+          icon={<Icon name='code' color='#ffffff' />}
+          buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+          title="Login" onPress={handleLogin} type="outline" />
+
+
+          <View style={{flexDirection:"row",justifyContent:"space-between",paddingTop:10}}>
+            <Text>Don't Have an account ? </Text> 
+            <TouchableOpacity onPress={handleView}>
+              <Text style={{color:"purple"}}> Register </Text>
+            </TouchableOpacity>
+          </View>
+                
+      </Card>
+        </View>
+              
+      </KeyboardAvoidingView>
+      
     );
   }
 
   else if (!user && view === "register") {
     return (
-      <View style={styles.contentContainer}>
+      <KeyboardAvoidingView behavior="height" style={styles.contentContainer}>
+<View style={{justifyContent:"center",flex:1}}>
+
+
+<Card
+  image={require('./assets/images/registerpage.jpg')}>
+  <View style={{marginBottom: 10}}>
+  <Text>Display Name</Text>
+  <Input
+    placeholder='Ahmad, Etc'
+    onChangeText={(text) => setDisplayName(text)}
+    inputStyle={{paddingLeft:10}}
+    leftIcon={
+      <Icon
+        type="AntDesign"
+        name='user'
+        color='lightgray'
+        size={20}
+      />
+    }
+    value={displayName}
+  />
+  <Text>Your Email Address</Text>
+  <Input
+    placeholder='email@address.com'
+    onChangeText={(text) => setEmail(text)}
+    value={email}
+    inputStyle={{paddingLeft:10}}
+    leftIcon={
+      <Icon
+        type="AntDesign"
+        name='cloud'
+        color='lightgray'
+        size={20}
+      />
+  }
+  />
+    <Text>Password</Text>
+    <Input
+    placeholder='password'
+    type="password"
+    secureTextEntry={true}
+    onChangeText={(text) => setPassword(text)}
+    inputStyle={{paddingLeft:10}}
+    leftIcon={
+      <Icon
+        type="FontAwesome"
+        name='lock'
+        color='lightgray'
+        size={20}
+      />
+    }
+    value={password}
+  />
+    <Text>Phone</Text>
+    <Input
+    placeholder='phone'
+    onChangeText={(text) => setPhone(text)}
+    inputStyle={{paddingLeft:10}}
+    keyboardType={'numeric'}
+    leftIcon={
+      <Icon
+        type="AntDesign"
+        name='phone'
+        color='lightgray'
+        size={20}
+      />
+    }
+    value={phone}
+  />
+  <Text>Department</Text>
+<Picker
+          selectedValue={department}
+          style={{height: 50, width: "100%"}}
+          onValueChange={(itemValue, itemIndex) =>
+            setDepartment(itemValue)
+          }>
+          <Picker.Item label="Select a Department" value="" />
+          <Picker.Item label="IT" value="IT" />
+          <Picker.Item label="business" value="business" />
+          <Picker.Item label="engineering" value="engineering" />
+          <Picker.Item label="health and science" value="health and science" />
+          <Picker.Item label="faculty" value="faculty" />
+        </Picker>
+
+  </View>
+  <Button
+    
+    icon={<Icon name='code' color='#ffffff' />}
+    buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+    title="Register" onPress={handleLogin} type="outline" />
+
+
+    <View style={{flexDirection:"row",justifyContent:"space-between",paddingTop:10}}>
+      <Text>Already Own an account ? </Text> 
+      <TouchableOpacity onPress={handleView}>
+        <Text style={{color:"purple"}}> Login </Text>
+      </TouchableOpacity>
+    </View>
+          
+</Card>
+  </View>
+
+
+
+
+
+      {/* <View style={{justifyContent:"center",flex:1}}>
         <TextInput
           style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
           onChangeText={setDisplayName}
@@ -145,47 +293,32 @@ export default function App(props) {
         />
 
         <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1,marginBottom:10 }}
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
           onChangeText={setPhone}
           placeholder="mobile number"
+          secureTextEntry={true}
           value={phone}
         />
-        {Platform.OS === "ios"? 
-          <>
-          <TouchableOpacity
-          onPress={() => {pickerRef.show()}}
-        >
-    <Text>{department === ""? "select a Department": department}</Text>
-        </TouchableOpacity>
-        <ReactNativePickerModule
-          pickerRef={e => (pickerRef = e)}
+
+        <Picker
           selectedValue={department}
-          title={'Select a department'}
-          items={['IT','business','engineering','health and science','faculty']}
-           style={{height: 50, width: "100%",}}
-          onCancel={() => {console.log("cancelled")}}
+          style={{height: 50, width: "100%"}}
           onValueChange={(itemValue, itemIndex) =>
             setDepartment(itemValue)
-          } 
-          />
-          </>
-        : 
-        <Picker
-        selectedValue={department}
-        style={{ height: 50, width: '100%' }}
-        onValueChange={(itemValue, itemIndex) => setDepartment(itemValue)}
-      >
-        <Picker.Item label="IT" value="IT" />
-        <Picker.Item label="business" value="business" />
-        <Picker.Item label="health and science" value="health and science" />
-        <Picker.Item label="faculty" value="faculty" />
-      </Picker>
-        }
-        
-        
+          }>
+          <Picker.Item label="Select a Department" value="" />
+          <Picker.Item label="IT" value="IT" />
+          <Picker.Item label="business" value="business" />
+          <Picker.Item label="engineering" value="engineering" />
+          <Picker.Item label="health and science" value="health and science" />
+          <Picker.Item label="faculty" value="faculty" />
+        </Picker>
+
+
         <Button title="submit" onPress={handleRegister} />
         <Button title="have and account? go to login" onPress={handleView} />
-      </View>
+      </View> */}
+      </KeyboardAvoidingView>
     );
   } else {
     return (
@@ -230,7 +363,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
   contentContainer: {
-    paddingTop: 30
+    paddingTop: 30,
+    flex:1
   },
   welcomeContainer: {
     alignItems: "center",
