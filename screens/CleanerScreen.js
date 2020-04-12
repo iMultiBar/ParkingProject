d0198a45af5c96642280a1f936bcb71a57c2b1c1
iMultiBar <shbshbal@gmail.com>
2020-04-12 13:03:47 +0300

    update cleaner

--- screens/CleanerScreen.js
+++ screens/CleanerScreen.js
@@ -7,61 +7,96 @@ import * as Animatable from 'react-native-animatable';
 import firebase from "firebase/app";
 import "firebase/auth";
 import db from "../db.js";
-
+import { Card, Input } from "react-native-elements";
 
 export default function CleanerScreen() {
-  const [jobs, setJobs] = useState([]);// this react hook useState uses to store a array of object which is the jobs
+  const [jobs, setJobs] = useState([]);
+  const [cleanerJobs, setCleanerJobs] = useState([])
+  const [flag, setFlag] = useState(true)
 
-  // this this react hook useEffect loads after everything loaded which
-  // uses a firebase query that listen to firebase in real time which it will loop though documents .each time a user request a clean.
-  // it will display the request immediately. which set the request inside the (jobs) object.
-  // also the firebase query only shows the request that other cleaners didn't took.
-  // by implementing an if statment which it will check if the cleanerUid is filled in the database.
-  // which will push the documents that have cleanerUid as null which means no one from the staff took it yet.
   useEffect(() =>{
+    getJobs()
+  },[])
+
+  const getJobs = async () =>{
     db.collection("requests").doc("clean").collection("cleanRequest").onSnapshot(querySnapshot => {
       const jobs = [];
+      const cleanerJobs = []
       querySnapshot.forEach(doc => {
         let check = doc.data()
         if(check.cleanerUid === null){
           jobs.push({ id: doc.id, ...doc.data() });
         }
+        else if(check.cleanerUid === firebase.auth().currentUser.uid && check.status != "done"){
+          cleanerJobs.push({id: doc.id, ...doc.data()})
+        }
       });
       console.log(" Current jobs: ", jobs);
       setJobs([...jobs]);
+      setCleanerJobs([...cleanerJobs])
     });
-  },[])
+  }
 
-
-  // this function have an arrgument called (n) which is the request object that the cleaner selected.
-  // also the function have firebase query which update the selected request with the cleaner UID which from it this reuqest now belongs to him/her.
   const takeCLeanRequest = (n) =>{
     db.collection("requests").doc("clean").collection("cleanRequest").doc(n.id).update({
       cleanerUid: firebase.auth().currentUser.uid,
       date: n.date,
       parkingLocation: n.parkingLocation,
       platNumber: n.platNumber,
-      status: n.status
+      status: "taken"
+    })
+  }
+  const completeCLeanRequest = (n) =>{
+    db.collection("requests").doc("clean").collection("cleanRequest").doc(n.id).update({
+      cleanerUid: firebase.auth().currentUser.uid,
+      date: n.date,
+      parkingLocation: n.parkingLocation,
+      platNumber: n.platNumber,
+      status: "done"
     })
   }
 
+
   return (
-    // this react native component is scrollView which helps the user to scroll between the reuqests 
+    flag ? 
     <ScrollView style={styles.container}>
+      <Button title="Profile" onPress={() =>setFlag(false)} />
       <View style={{flex:4}}>
-        {/* this map functions loops inside jobs array of objects which display all the available requests.  */}
+        
       {jobs.map((n,i) => (
-        // this react native component called Animatable which animate a upside down animation that cheers the users and make them happy.or sad :)
+       
         <Animatable.View animation="slideInDown" iterationCount={3} direction="alternate">
 
-          <View key={i} style={{borderColor:"black",borderWidth:3,borderStyle:"solid", marginBottom:20,padding:5}}>
+          <Card key={i} style={{borderColor:"black",borderWidth:3,borderStyle:"solid", marginBottom:20,padding:5}}>
           <Text><Text style={{ fontWeight: 'bold' }}>Date</Text>: {n.date}</Text>
           <Text><Text style={{ fontWeight: 'bold' }}>parking location</Text>: {n.parkingLocation}</Text>
           <Text><Text style={{ fontWeight: 'bold' }}>plat number</Text>: {n.platNumber}</Text>
-          <Text><Text style={{ fontWeight: 'bold' }}>Status</Text>: {n.status? "true": "false"}</Text>
+          <Text><Text style={{ fontWeight: 'bold' }}>Status</Text>: {n.status? "true": "avaliable"}</Text>
           {/* this button the triggers the functions which it explaned apove */}
           <Button title="Take the request" onPress={() =>takeCLeanRequest(n)} />
-        </View>
+        </Card>
+          </Animatable.View>
+        
+      ))}
+      </View>
+    </ScrollView>
+    :
+    <ScrollView style={styles.container}>
+      <Button title="Back" onPress={() =>setFlag(true)} />
+      <View style={{flex:4}}>
+        
+      {cleanerJobs.map((n,i) => (
+       
+        <Animatable.View animation="slideInDown" iterationCount={3} direction="alternate">
+
+          <Card key={i} style={{borderColor:"black",borderWidth:3,borderStyle:"solid", marginBottom:20,padding:5}}>
+          <Text><Text style={{ fontWeight: 'bold' }}>Date</Text>: {n.date}</Text>
+          <Text><Text style={{ fontWeight: 'bold' }}>parking location</Text>: {n.parkingLocation}</Text>
+          <Text><Text style={{ fontWeight: 'bold' }}>plat number</Text>: {n.platNumber}</Text>
+          <Text><Text style={{ fontWeight: 'bold' }}>Status</Text>: {n.status}</Text>
+          {/* this button the triggers the functions which it explaned apove */}
+          <Button title="Request is done" onPress={() =>completeCLeanRequest(n)} />
+        </Card>
           </Animatable.View>
         
       ))}
